c1caba2a96b04b4e4290f75892b019c0201c2424
Eric Tran <eric.tran@chime.com>
2024-09-26 23:08:33 -0400

    clean install, no hotkeys

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,5 @@
 return {
     "numToStr/Comment.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
     config = function() require("Comment").setup() end
 }
