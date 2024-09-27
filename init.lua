9b418e54562e30f0c2c83abf4d4513309744eb3f
Eric Tran <eric.tran@chime.com>
2024-09-27 16:00:39 -0400

    add hotkeys

--- init.lua
+++ init.lua
@@ -1,19 +1,116 @@
 require("config.lazy")
-print("hello")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
 
 -- keymaps
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+-- local discipline = require("plugins.discipline")
+--
+-- discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+keymap.set("n", "<leader>r", function()
+  require("craftzdog.hsl").replaceHexWithHSL()
+end)
+
+keymap.set("n", "<leader>i", function()
+  require("craftzdog.lsp").toggleInlayHints()
+end)
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
     -- Remove blank lines between import statements
-    local save_cursor = vim.fn.getpos(".")  -- Save the cursor position
-    vim.cmd([[silent! g/^import (\_.\{-})$/s/\n\s*\n/\r/g]])  -- Remove blank lines within import block
-    vim.fn.setpos('.', save_cursor)         -- Restore the cursor position
+    local save_cursor = vim.fn.getpos(".")                   -- Save the cursor position
+    vim.cmd([[silent! g/^import (\_.\{-})$/s/\n\s*\n/\r/g]]) -- Remove blank lines within import block
+    vim.fn.setpos('.', save_cursor)                          -- Restore the cursor position
 
     local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
+    params.context = { only = { "source.organizeImports" } }
     -- buf_request_sync defaults to a 1000ms timeout. Depending on your
     -- machine and codebase, you may want longer. Add an additional
     -- argument after params if you find that you have to write the file
@@ -28,6 +125,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         end
       end
     end
-    vim.lsp.buf.format({async = false})
+    vim.lsp.buf.format({ async = false })
   end
 })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,60 +1,97 @@
-return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            enabled = true
-        }, {"nvim-telescope/telescope-file-browser.nvim", enabled = true}
-    },
-    branch = "0.1.x",
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local Path = require('plenary.path')
 
-        telescope.setup({
-            defaults = {
-                sorting_strategy = "ascending",
-                layout_strategy = "horizontal",
-                layout_config = {prompt_position = "top"},
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                        ["<C-j>"] = actions.move_selection_next, -- move to next result
-                        ["<C-q>"] = actions.send_selected_to_qflist +
-                            actions.open_qflist -- send selected to quickfixlist
-                    }
-                }
-            },
-            extensions = {
-                file_browser = {
-                    path = "%:p:h", -- open from within the folder of your current buffer
-                    display_stat = false, -- don't show file stat
-                    grouped = true, -- group initial sorting by directories and then files
-                    hidden = true, -- show hidden files
-                    hide_parent_dir = true, -- hide `../` in the file browser
-                    hijack_netrw = true, -- use telescope file browser when opening directory paths
-                    prompt_path = true, -- show the current relative path from cwd as the prompt prefix
-                    use_fd = true -- use `fd` instead of plenary, make sure to install `fd`
-                }
-            }
-        })
+local delete_file = function(prompt_bufnr)
+  local selected_entry = action_state.get_selected_entry()
+  local current_picker = action_state.get_current_picker(prompt_bufnr)
+  local filepath = selected_entry[1]
 
-        telescope.load_extension("fzf")
-        telescope.load_extension("file_browser")
+  -- Confirm deletion
+  vim.ui.input({ prompt = 'Delete file? (y/n): ' }, function(input)
+    if input == 'y' then
+      -- Delete the file
+      local p = Path:new(filepath)
+      p:unlink()
 
-        local builtin = require("telescope.builtin")
-
-        -- key maps
-
-        local map = vim.keymap.set
-        local opts = {noremap = true, silent = true}
-
-        map("n", "-", ":Telescope file_browser<CR>")
-
-        map("n", "<leader>ff", builtin.find_files, opts) -- Lists files in your current working directory, respects .gitignore
-        map("n", "<leader>fx", builtin.treesitter, opts) -- Lists tree-sitter symbols
-        map("n", "<leader>fs", builtin.spell_suggest, opts) -- Lists spell options
+      -- Refresh the picker to show the file is deleted
+      current_picker:refresh()
     end
+  end)
+end
+
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      enabled = true
+    }, { "nvim-telescope/telescope-file-browser.nvim", enabled = true }
+  },
+  branch = "0.1.x",
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist +
+                actions.open_qflist,               -- send selected to quickfixlist
+            ["<C-d>"] = delete_file,
+          }
+        }
+      },
+      extensions = {
+        file_browser = {
+          path = "%:p:h",    -- open from within the folder of your current buffer
+          display_stat = false, -- don't show file stat
+          grouped = true,    -- group initial sorting by directories and then files
+          hidden = true,     -- show hidden files
+          hide_parent_dir = true, -- hide `../` in the file browser
+          hijack_netrw = true, -- use telescope file browser when opening directory paths
+          prompt_path = true, -- show the current relative path from cwd as the prompt prefix
+          use_fd = true      -- use `fd` instead of plenary, make sure to install `fd`
+        }
+      }
+    })
+
+    telescope.load_extension("fzf")
+    telescope.load_extension("file_browser")
+
+    local builtin = require("telescope.builtin")
+    -- key maps
+    -- keys = {
+    --   keymap.set("n", "<C-p>", ":Telescope git_files<CR>", keymap_options({ desc = "Find files (Git-aware)" })),
+    --   keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", keymap_options({ desc = "Find files" })),
+    --   keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", keymap_options({ desc = "Find buffers" })),
+    --   keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>", keymap_options({ desc = "Find keymaps" })),
+    --   keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", keymap_options({ desc = "Find help tags" })),
+    --   keymap.set("n", "<leader>fa", ":Telescope <CR>", keymap_options({ desc = "Find all" })),
+    --   keymap.set("n", "<leader>fm", ":Telescope marks<CR>", keymap_options({ desc = "Find marks" })),
+    --   keymap.set("n", "<leader>fo", ":Telescope oldfiles<CR>", keymap_options({ desc = "Find previously openfiles" })),
+    --   keymap.set("n", "<leader>fv", ":Telescope vim_options<CR>", keymap_options({ desc = "Find vim options" })),
+    --   keymap.set("n", "<leader>fy", ":Telescope filetypes<CR>", keymap_options({ desc = "Find available filetypes" })),
+    -- },
+    local map = vim.keymap.set
+    local opts = { noremap = true, silent = true }
+    -- builtin.lsp_definitions({ reuse_win = false })
+
+    map("n", "-", ":Telescope file_browser<CR>")
+
+    map("n", "<leader>fb", builtin.buffers, opts)  -- Lists open buffers
+    map("n", "<leader>fm", builtin.marks, opts)    -- Lists marks
+    map("n", "<leader>fd", builtin.find_files, opts) -- Lists files in your current working directory, respects .gitignore
+    map("n", "<leader>fg", builtin.live_grep, opts) -- Lists files in your current working directory, respects .gitignore
+    map("n", "<leader>fx", builtin.treesitter, opts) -- Lists tree-sitter symbols
+    map("n", "<leader>fs", builtin.live_grep, opts) -- Lists spell options
+  end
 }
